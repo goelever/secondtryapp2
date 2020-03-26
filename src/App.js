7c46a159e69eaa94d378eef8927e77ff673b3d9a
goelever <stefan.hartweg@web.de>
2020-03-26 12:58:52 +0100

    asyncAwaitAdded

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 
 
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -16,32 +17,31 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-
+    //const logger = new Logger('foo');
+    try {
     fetch('http://jsonplaceholder.typicode.com/users')
       .then(res => res.json(), this.setState({ didfetch: true }))
       .then((data) => {
         this.setState({ contacts: data })
       },
-        this.setState({ didsetState: true })).catch(error => {
-          console.log("error.response from put: ", error.response)
-        })
-
+        this.setState({ didsetState: true })) 
+        
+    }catch(e) {
+         // logger.error('error happened', e);
+        } 
           this.setState({ didmount: true })
     }
   //.catch(console.log)
   render() {
-    const socialMediaList = this.state.contacts;//data.Experiences;//
-    var first = socialMediaList[0];
+
     return (
       <ul>
-        Hat geklappt1234 8999!
-        {/* {socialMediaList.map(s => (<li>{s.companyName}</li>))} */}
-        {<li>Laenge socialmedia: {socialMediaList.length}</li>}
-
-        {<li>didfetch : {this.state.didfetch.toString()} </li>}
+        Rendering...
+        {<li>Length data:: {this.state.contacts.length}</li>}
+        {<li>didfetch :: {this.state.didfetch.toString()} </li>}
         {<li>didsetState : {this.state.didsetState.toString()} </li>}
         {<li>Didmount : {this.state.didmount.toString()} </li>}
-        {this.state.contacts.map(s => (<li>{s.id}:{s.name}:Website: {s.website}</li>))}
+        {this.state.contacts.map(s => (<li>{s.id}: {s.website}</li>))}
       </ul>
     );
   }
